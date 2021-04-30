976fd066db7317a0d7fb3a9605031e24f8f879ca
Shuliak Leonid <zeleny777@gmail.com>
2021-04-30 20:17:12 +0300

    add login page

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import './Login.scss'
 
 const Login = () => {
-    return (
-        <div>
 
+  return (
+      <div className="container">
+        <div className="box">
+          <h2>Login</h2>
+          <form action="">
+            <div className="inputBox">
+              <input type="text" required={false}/>
+              <label>User Name</label>
+            </div>
+            <div className="inputBox">
+              <input type="password" required={false}/>
+              <label>Password</label>
+            </div>
+            <input type="submit" value="Submit"/>
+          </form>
         </div>
-    );
+      </div>
+  )
 };
 
 export default Login;
