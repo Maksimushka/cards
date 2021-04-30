96b262eee8e91df791106271ce5ec643c208727f
Shuliak Leonid <zeleny777@gmail.com>
2021-04-30 21:56:29 +0300

    add validation from login and password

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import './Login.scss'
 
-const Login = () => {
+const Login: FC<any> = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [validEmail, setValidEmail] = useState<boolean>(false)
+  const [validPassword, setValidPassword] = useState<boolean>(false)
+
+
+  const changeEmail = ({target}: ChangeEvent<HTMLInputElement>) => {
+    setValidEmail(false)
+    setEmail(target.value)
+  }
+
+  const changePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setValidPassword(false)
+    setPassword(e.target.value)
+  }
+  const onSubmit = () => {
+    const pattern = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+    if (!pattern.test(email)) {
+      setValidEmail(true)
+    }
+    if (password.length < 6) {
+      setValidPassword(true)
+    }
+  }
 
   return (
       <div className="container">
@@ -9,14 +33,16 @@ const Login = () => {
           <h2>Login</h2>
           <form action="">
             <div className="inputBox">
-              <input type="text" required={false}/>
-              <label>User Name</label>
+              <input type="text" required={true} value={email} onChange={changeEmail}/>
+              <label>Email</label>
             </div>
             <div className="inputBox">
-              <input type="password" required={false}/>
+              <input type="password" required={true} value={password} onChange={changePassword}/>
               <label>Password</label>
             </div>
-            <input type="submit" value="Submit"/>
+            {validEmail && <p className="error">Enter valid email</p>}
+            {validPassword && <p className="error">Enter longer password more then 6 symbols</p>}
+            <input type="submit" value="Submit" onClick={onSubmit}/>
           </form>
         </div>
       </div>
