8e9593a8562c6e0f8516414b0b8ad427e36f276d
Maximushka <m.billsrc@gmail.com>
2021-05-02 19:03:32 +0700

    Добавлена структура

--- src/features/recoveryPass/RecoveryPass.tsx
+++ src/features/recoveryPass/RecoveryPass.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Input from '../../components/input/Input';
 
 const RecoveryPass = () => {
     return (
         <div>
-
+            <h2>Recovery password</h2>
+            <label htmlFor='recov'>Enter your email:</label>
+            <Input id='recov'/>
         </div>
     );
 };
